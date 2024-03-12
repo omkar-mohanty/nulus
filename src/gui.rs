d2630441c064dfefa2059a6b85290aaa320ba792
omkar-mohanty <franzohouser@gmail.com>
2024-03-12 13:13:18 +0530

    Initit

---
+++ src/gui.rs
@@ -0,0 +1,16 @@
+use egui::{Align2, Context};
+
+pub fn nullus_gui(ctx: &Context) {
+    egui::Window::new("Nullus")
+        .default_open(true)
+        .max_width(1000.0)
+        .max_height(800.0)
+        .default_width(800.0)
+        .resizable(true)
+        .anchor(Align2::LEFT_TOP, [0.0, 0.0])
+        .show(&ctx, |ui| {
+            ui.end_row();
+
+            // proto_scene.egui(ui);
+        });
+}
